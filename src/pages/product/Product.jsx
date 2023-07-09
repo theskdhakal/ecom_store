234de1cc116fa346c4c05a025609f40888353f02
Shiva Dhakal <theskdhakal@gmail.com>
2023-07-09 14:09:40 +1000

    add new product section added

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { UserLayout } from "../../components/layout/user-layout/UserLayout";
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
 export const Product = () => {
   return (
     <UserLayout>
       <div>Product</div>
+
+      <hr />
+      <div className="text-end mb-3">
+        <Link to="/">
+          <Button variant="primary">+Add New Product</Button>
+        </Link>
+      </div>
     </UserLayout>
   );
 };
